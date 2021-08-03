b373fb36beb534615f100b7076c9b4d0e5281031
sameh97 <sameh7462@gmail.com>
2021-08-03 18:00:20 +0300

    added crud operations to the ui

--- src/app/components/scheduler-components/add-scheduled-job-page/add-scheduled-job-page.component.ts
+++ src/app/components/scheduler-components/add-scheduled-job-page/add-scheduled-job-page.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AppUtil } from 'src/app/common/app-util';
 import { ScheduledJob } from 'src/app/model/scheduled-job';
 import { SchedulerService } from 'src/app/services/scheduler-service/scheduler.service';
 import { Subscription } from 'rxjs';
-
+import { MachinesService } from './../../../services/machines-service/machines.service';
+import { Machine } from 'src/app/model/machine';
+import { filter, switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-add-scheduled-job-page',
   templateUrl: './add-scheduled-job-page.component.html',
@@ -17,7 +24,8 @@ export class AddScheduledJobPageComponent implements OnInit, OnDestroy {
 
   constructor(
     private formBuilder: FormBuilder,
-    private schedulerService: SchedulerService
+    private schedulerService: SchedulerService,
+    private machinesService: MachinesService
   ) {}
 
   ngOnInit(): void {
@@ -32,6 +40,7 @@ export class AddScheduledJobPageComponent implements OnInit, OnDestroy {
           Validators.required,
           Validators.minLength(4),
           Validators.maxLength(50),
+          this.machineExistValidator,
         ],
       ],
       startTime: ['', [Validators.required]],
@@ -40,6 +49,31 @@ export class AddScheduledJobPageComponent implements OnInit, OnDestroy {
       isActive: ['', [Validators.required]],
     });
   }
+
+  private machineExistValidator = (
+    control: AbstractControl
+  ): { [key: string]: any } | null => {
+    let machineToCheck: Machine = null;
+
+    this.subscriptions.push(
+      this.machinesService
+        .getBySerialNumber(control.value)
+        .pipe(filter((machine) => AppUtil.hasValue(machine)))
+        .subscribe((machine) => {
+          machineToCheck = machine;
+        })
+    );
+
+    // return AppUtil.hasValue(machineToCheck)
+    //    null
+    //   : { machineNotExist: { value: control.value } };
+
+    return null;
+  };
+
+  // if (!AppUtil.hasValue(machineToCheck)) {
+  //   return { machineNotExist: { value: control.value } };
+  // }
 
   ngOnDestroy(): void {
     AppUtil.releaseSubscriptions(this.subscriptions);
