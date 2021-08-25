60251df6d108c770523fe0e6917802ca520450c5
sameh97 <sameh7462@gmail.com>
2021-08-25 14:41:00 +0300

    added upload file service

--- src/app/common/consts.ts
+++ src/app/common/consts.ts
@@ -2,7 +2,8 @@ export class AppConsts {
   public static BASE_URL = 'http://localhost:3000';
   public static readonly KEY_USER_TOKEN = 'KEY_USER_TOKEN';
   public static readonly STOMP_ENDPOINT = '/easyfit-websocket-endpoint';
-
+  public static readonly FORM_DATA_FILE_UPLOAD_KEY = 'file';
+  public static readonly FORM_DATA_FILE_DELETE_KEY = 'url';
   public static readonly SESSION_EXPIRED_ERROR =
     'Session expired please login again';
 }
